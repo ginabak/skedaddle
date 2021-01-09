21189752364b30371285ca7c796a3f342dd3ae74
ginabak <gina@ginabak.com>
2021-01-09 18:33:22 -0500

    Add array in Carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react';
 import Slider from "react-slick";
+import { MediaCard } from '@shopify/polaris';
 import './Carousel.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -10,26 +11,41 @@ export function Carousel() {
       function SampleNextArrow(props) {
             const { className, style, onClick } = props;
             return (
-              <div
-                className={className}
-                style={{ ...style, display: "block", background: "red" }}
-                onClick={onClick}
-              />
+                  <div
+                        className={className}
+                        style={{ ...style, display: "block", background: "red" }}
+                        onClick={onClick}
+                  />
             );
-          }
-          
-          function SamplePrevArrow(props) {
+      }
+
+      function SamplePrevArrow(props) {
             const { className, style, onClick } = props;
             return (
-              <div
-                className={className}
-                style={{ ...style, display: "block", background: "blue" }}
-                onClick={onClick}
-              />
+                  <div
+                        className={className}
+                        style={{ ...style, display: "block", background: "blue" }}
+                        onClick={onClick}
+                  />
             );
-          }
-      const storesExclude = ["https://us.organicbasics.com/", "https://communitythriftandvintage.com/", "https://teamtimbuktu.com/", "https://aliceandwhittles.com/"];
-      const stores = ["https://kotn.com/", "https://www.hazlewoodcompany.com/", "https://www.mightygoodbasics.com/"];
+      }
+      const storesExclude = ["https://kotn.com/", "https://www.hazlewoodcompany.com/", "https://www.mightygoodbasics.com/", "https://us.organicbasics.com/", "https://communitythriftandvintage.com/", "https://teamtimbuktu.com/", "https://aliceandwhittles.com/"];
+      const stores = [
+            "https://readytowearagain.com/",
+            "https://www.urbanoutfitters.com/en-ca/brands/urban-renewal",
+            "https://communitythriftandvintage.com/",
+            "https://wornwear.patagonia.com/",
+            "https://www.hazlewoodcompany.com/",
+      ];
+      const storeNames = [
+            "Ready To Wear Again",
+            "Urban Renewal",
+            "Community Thrift and Vintage",
+            "Worn Wear Patagonia",
+            "Hazlewood",
+            ""
+      ]
+
       const settings = {
             dots: true,
             infinite: true,
@@ -44,11 +60,15 @@ export function Carousel() {
       return (
             <div>
                   <Slider {...settings}>
-                        {stores.map((url) => {
+                        {stores.map((url,index) => {
                               return (
-                                    <div>
-                                          <iframe src={url} width="700" height="500" frameBorder="0" />
-                                    </div>
+                                    <MediaCard
+                                          title={storeNames[index]}
+                                          primaryAction={{
+                                                content: 'Shop',
+                                                onAction: (url) => {},
+                                          }}                                    >
+                                    </MediaCard>
                               );
                         })}
 
