9467cb1e59b9ff828b7871858e8d4893a473cb49
ginabak <gina@ginabak.com>
2021-01-09 15:28:35 -0500

    Add polaris

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -6,22 +6,48 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export function Carousel() {
+
+      function SampleNextArrow(props) {
+            const { className, style, onClick } = props;
+            return (
+              <div
+                className={className}
+                style={{ ...style, display: "block", background: "red" }}
+                onClick={onClick}
+              />
+            );
+          }
+          
+          function SamplePrevArrow(props) {
+            const { className, style, onClick } = props;
+            return (
+              <div
+                className={className}
+                style={{ ...style, display: "block", background: "green" }}
+                onClick={onClick}
+              />
+            );
+          }
+      const storesExclude = ["https://us.organicbasics.com/", "https://communitythriftandvintage.com/", "https://teamtimbuktu.com/", "https://aliceandwhittles.com/"];
+      const stores = ["https://kotn.com/", "https://www.hazlewoodcompany.com/", "https://www.mightygoodbasics.com/"];
       const settings = {
             dots: true,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            nextArrow: <SampleNextArrow />,
+            prevArrow: <SamplePrevArrow />
+
       };
-      const storesExclude = ["https://us.organicbasics.com/","https://teamtimbuktu.com/", "https://aliceandwhittles.com/"];
-      const stores = ["https://kotn.com/", "https://www.mightygoodbasics.com/"];
+
       return (
             <div>
                   <Slider {...settings}>
                         {stores.map((url) => {
                               return (
                                     <div>
-                                         <iframe src={url} width="700" height="500" frameBorder="0"/>
+                                          <iframe src={url} width="700" height="500" frameBorder="0" />
                                     </div>
                               );
                         })}
