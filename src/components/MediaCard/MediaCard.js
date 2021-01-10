8b12efe6f7ab95bc5b0db19cfbee20aa976f979d
ginabak <gina@ginabak.com>
2021-01-10 01:51:13 -0500

    Fix isClick

--- src/components/MediaCard/MediaCard.js
+++ src/components/MediaCard/MediaCard.js
@@ -1,42 +1,40 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
 import ShareIcon from "@material-ui/icons/Share";
 import Heart from "react-animated-heart";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: "100%",
-    fontFamily: 'Arapey'
-  },
-  media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
-}));
+
 
 export function MediaCard({ url, description, image, name }) {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: "100%",
+      fontFamily: 'Arapey'
+    },
+    media: {
+      height: 0,
+      paddingTop: "56.25%", // 16:9
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: "rotate(180deg)",
+    },
+  }));
   const classes = useStyles();
-  const [isClick, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
   return (
     <Card className={classes.root}>
@@ -60,7 +58,7 @@ export function MediaCard({ url, description, image, name }) {
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
+        <Heart isClick={click} onClick={() => setClick(!click)} />
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
