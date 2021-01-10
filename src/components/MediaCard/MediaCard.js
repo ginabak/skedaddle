a31fd75950da95579e1088235912afc380d85196
Jalena Lee <jalena.lee@shopify.com>
2021-01-09 21:19:51 -0500

    heart

--- src/components/MediaCard/MediaCard.js
+++ src/components/MediaCard/MediaCard.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Heart from "react-animated-heart";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,24 +22,22 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
   avatar: {
     backgroundColor: red[500],
   },
 }));
-
-
 
 export function MediaCard({ url, description, image, name }) {
   const classes = useStyles();
@@ -49,31 +48,31 @@ export function MediaCard({ url, description, image, name }) {
   };
   // const openSite = useCallback((website) => { window.open(website, "_blank") },);
 
+  const [isClick, setClick] = useState(false);
 
   return (
     <Card className={classes.root}>
       <CardHeader
         action={
-          <IconButton aria-label="settings" onClick={() => { window.open(url, "_blank") }}>
+          <IconButton
+            aria-label="settings"
+            onClick={() => {
+              window.open(url, "_blank");
+            }}
+          >
             shop
           </IconButton>
         }
         title={name}
       />
-      <CardMedia
-        className={classes.media}
-        image={image}
-        title="Paella dish"
-      />
+      <CardMedia className={classes.media} image={image} title="Paella dish" />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
+        <Heart isClick={isClick} onClick={() => setClick(!isClick)} />
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
@@ -84,14 +83,12 @@ export function MediaCard({ url, description, image, name }) {
           onClick={handleExpandClick}
           aria-expanded={expanded}
           aria-label="show more"
-        >
-        </IconButton>
+        ></IconButton>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-        </CardContent>
+        <CardContent></CardContent>
       </Collapse>
-    </Card >
+    </Card>
   );
 }
 
